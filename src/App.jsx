ff8eac669dec61c8d14d3d9e7bd142b9c7ce94dc
Thiglfa <thiagllima26@gmail.com>
2024-08-16 11:21:39 -0300

    primeiros exemplos de react

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Componente1 from "./components/Componente1"
+
+function App() {
+
+  const aluno = "João"
+
+
+  return (
+    <>
+      <h1>Primeira Aula</h1>
+      <p>Nossa primeira aula</p>
+      <p>O aluno {aluno}</p>
+      <Componente1 />
+    </>
+    
+
+  )
+}
+
+export default App
---
+++ src/components/Componente1.jsx
@@ -0,0 +1,14 @@
+import logoreact from './../assets/react.svg'
+
+function Componente1(){
+    
+    return (
+        <>
+        <h2>Componente1</h2>
+        <img src={logoreact} alt="logo React" />
+        <img src="vite.svg" alt="logo do vite" />
+        </>
+    )
+}
+
+export default Componente1
